a7910004e780c7f4046d596cade52e8fa78016d1
Jitender Kumar <thejitenderk@gmail.com>
2025-05-06 01:04:28 -0400

    added backend block

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.27.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "ecomstorageaccountdev"
+    container_name       = "tfstate"
+    key                 = "dev.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
